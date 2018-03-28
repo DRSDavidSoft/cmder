24f6d03940f5da3b2a183db666279d8b8af2034a
David Refoua <David@Refoua.me>
2018-03-28 21:11:31 +0430

    add verbose version info

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -85,6 +85,7 @@ if ($Compile) {
     if ($LastExitCode -ne 0) {
         throw "MSBuild failed to build the launcher executable."
     }
+    else Write-Verbose "successfully built Cmder $Version!"
     Pop-Location
 }
 
