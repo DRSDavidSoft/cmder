d5b8a87d33186317f4b728b7334fc8fd65a67144
Dax T. Games <dtgames@kinggeek.org>
2020-06-07 15:32:15 -0400

    Allow setting cmder init.bat arg vars globally on windows.

--- vendor/init.bat
+++ vendor/init.bat
@@ -52,6 +52,12 @@ call "%cmder_root%\vendor\lib\lib_path"
 call "%cmder_root%\vendor\lib\lib_git"
 call "%cmder_root%\vendor\lib\lib_profile"
 
+if "%CMDER_CONFIGURED%" == "1" (
+  echo Cmder is already configured, skipping to user config!
+
+  goto USER_CONFIG_START
+)
+
 :var_loop
     if "%~1" == "" (
         goto :start
